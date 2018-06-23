7ba7090feaba13f1ade63e98b22995b1e9b91622
CGS-Jack-Bashford <jack.bashford42@gmail.com>
2018-06-23 10:41:43 +1000

    Changed contact form

--- Site/PHP/contactMe.php
+++ Site/PHP/contactMe.php
@@ -9,7 +9,7 @@ $date = getdate();
 $to = "jack.bashford42@gmail.com";
 $email_subject = "$name wants to talk to you"
 $email_body = "$emailBody \n \n Sent from cgs-jack-bashford.github.io at $date";
-$headers = 
+$headers = "From: $to";
 
 mail($to,$subject,$body);
 
