b8ec3a622d470dcfb156c831c7cf9d25583c050e
CGS-Jack-Bashford <jack.bashford42@gmail.com>
2018-06-23 10:46:29 +1000

    contact

--- Site/PHP/contactMe.php
+++ Site/PHP/contactMe.php
@@ -22,10 +22,9 @@ $email = $_POST['email'];
 $talkAbout = $_POST['talkAbout'];
 $emailBody = $_POST['emailBody'];
 
-$date = getdate();
 $to = "jack.bashford42@gmail.com";
 $email_subject = "$name wants to talk to you"
-$email_body = "$emailBody \n \n Sent from cgs-jack-bashford.github.io at $date";
+$email_body = "$emailBody \n \n Sent from cgs-jack-bashford.github.io at a time.";
 $headers = "From: $to";
 
 mail($to,$subject,$body);
